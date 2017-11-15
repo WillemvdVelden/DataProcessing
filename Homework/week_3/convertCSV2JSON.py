a03348be09dffc56d84aa4dfb3cad4988f738e0e
WillemvdVelden <willem.vandervelden1995@gmail.com>
2017-11-15 12:57:55 +0100

    homework week 3 part 1

---
+++ Homework/week_3/convertCSV2JSON.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# convertCSV2JSON.py
+# Name: Willem van der Velden
+# Student number: 10546324
+'''
+This script converts data from a .csv file into JSON
+'''
+
+import csv
+import json
+
+# initialize empty list
+data = []
+
+# open csv file and append data to list
+with open('dataset.csv', newline='') as f:
+    reader = csv.reader(f, delimiter=';')
+    for row in reader:
+        data.append(row)
+
+# dump data from list into json file
+with open('dataset.json', 'w') as outfile:
+	json.dump(data, outfile)
+
+
