129b40e239449192ae0da45aa89c516179b90402
Willem van der Velden <31651636+WillemvdVelden@users.noreply.github.com>
2017-11-16 18:43:29 +0100

    Update

--- Homework/week_3/barchart.js
+++ Homework/week_3/barchart.js
@@ -6,34 +6,36 @@
 *	barchart.js
 */
 
-var width = 420,
-    barHeight = 20;
+var width = 960,
+    height = 500
 
-var x = d3.scale.linear()
-    .range([0, width]);
+var y = d3.scale.linear()
+    .range([height, 0]);
 
 var chart = d3.select(".chart")
-    .attr("width", width);
+    .attr("width", width)
+    .attr("height", height);
 
 // load the data
 d3.json("dataset.json", function(error, data) {
-	console.log(typeof(data[0]['rain']));
-	x.domain([0, d3.max(data, function(d) { return d.rain; })]);
+	console.log(data);
+	y.domain([0, d3.max(data, function(d) { return d.rain; })]);
 
-	chart.attr("height", barHeight * data.length);
+	var barWidth = width / data.length
 
 	var bar = chart.selectAll("g")
 	  .data(data)
 	  .enter().append("g")
-	  .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
+	  .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
 
 	bar.append("rect")
-	  .attr("width", function(d) { return x(d.rain); })
-	  .attr("height", barHeight - 1);
+	  .attr("y", function(d) { return y(d.rain); })
+	  .attr("height", function(d) { return height - y(d.rain); })
+      .attr("width", barWidth - 1);
 
 	bar.append("text")
-	  .attr("x", function(d) { return x(d.rain) - 3; })
-	  .attr("y", barHeight / 2)
-	  .attr("dy", ".35em")
-	  .text(function(d) { return d.rain; });
+      .attr("x", barWidth / 2)
+      .attr("y", function(d) { return y(d.rain) + 3; })
+      .attr("dy", ".75em")
+      .text(function(d) { return d.rain; });
 });
